80306f9456a4eee46778f9c4cb09fc31eb825588
jsantoso2 <44449948+jsantoso2@users.noreply.github.com>
2020-08-16 21:25:13 -0400

    Add files via upload

---
+++ app/firebase-upload/config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyDyysTAMfu-eEFjo-nOKfc5XqtWATsiGzM",
+    authDomain: "yelp-sentiment-project.firebaseapp.com",
+    databaseURL: "https://yelp-sentiment-project.firebaseio.com",
+    projectId: "yelp-sentiment-project",
+    storageBucket: "yelp-sentiment-project.appspot.com",
+    messagingSenderId: "418790205387",
+    appId: "1:418790205387:web:a64741ff32b99a6884b84e"
+}
+// Exports
+module.exports = firebaseConfig;
---
+++ app/firebase-upload/import.js
@@ -0,0 +1,23 @@
+// Imports
+const firestoreService = require('firestore-export-import');
+const firebaseConfig = require('./config.js');
+const serviceAccount = require('./serviceAccount.json');
+
+// JSON To Firestore
+const jsonToFirestore = async () => {
+  try {
+    console.log('Initialzing Firebase');
+    await firestoreService.initializeApp(serviceAccount, firebaseConfig.databaseURL);
+    console.log('Firebase Initialized');
+
+    await firestoreService.restore('../../dataset/business_firebase.json');
+    await firestoreService.restore('../../dataset/ratings_firebase.json');
+    await firestoreService.restore('../../dataset/users_firebase.json');
+    console.log('Upload Success');
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+jsonToFirestore();
